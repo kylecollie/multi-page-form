96b3afedb517c57ee4029b7e65b3580ca44aa9a1
Kyle Collie <kcollie@gmail.com>
2024-12-04 07:44:49 -0500

    adds dynamic form funtion

--- src/app/add/step-three/StepThreeForm.tsx
+++ src/app/add/step-three/StepThreeForm.tsx
@@ -2,16 +2,28 @@
 
 import Input from '@/components/Input';
 import SubmitButton from '@/components/Submit';
+import { stepThreeFormAction } from './actions';
+import { useFormState } from 'react-dom';
+
+const initialState: FormErrors = {};
 
 export default function StepThreeForm() {
+  const [serverErrors, formAction] = useFormState(
+    stepThreeFormAction,
+    initialState
+  );
   return (
-    <form className="flex flex-1 flex-col items-center">
+    <form
+      action={formAction}
+      className="flex flex-1 flex-col items-center"
+    >
       <div className="flex w-full flex-col gap-8 lg:max-w-[700px] ">
         <Input
           label="Contact Name"
           id="contactName"
           type="text"
           required
+          errorMsg={serverErrors?.contactName}
           minLength={5}
         />
         <Input
@@ -19,6 +31,7 @@ export default function StepThreeForm() {
           id="contactEmail"
           type="email"
           required
+          errorMsg={serverErrors?.contactEmail}
         />
         <SubmitButton text="Submit" />
       </div>
--- src/app/add/step-three/actions.ts
+++ src/app/add/step-three/actions.ts
@@ -1,3 +1,26 @@
 'use server';
 
-export const stepThreeFormAction = () => {};
+import { stepThreeSchema } from '@/schemas';
+import { AddDealRoutes, FormErrors } from '@/types';
+import { redirect } from 'next/navigation';
+
+export const stepThreeFormAction = (
+  prefState: FormErrors | undefined,
+  formData: FormData
+): FormErrors | undefined => {
+  const data = Object.fromEntries(formData.entries());
+  const validated = stepThreeSchema.safeParse(data);
+
+  if (!validated.success) {
+    const errors = validated.error.issues.reduce(
+      (acc: FormErrors, issue) => {
+        acc[issue.path[0]] = issue.message;
+        return acc;
+      },
+      {}
+    );
+    return errors;
+  }
+
+  redirect(AddDealRoutes.REVIEW_DEAL);
+};
