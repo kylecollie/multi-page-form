34e14e0f159550bb976ebf24e5d3c32abdfbe57b
Kyle Collie <kcollie@gmail.com>
2024-12-09 13:11:52 -0500

    Finished

--- src/app/add/review/ReviewForm.tsx
+++ src/app/add/review/ReviewForm.tsx
@@ -1,13 +1,45 @@
 'use client';
 
+import SubmitButton from '@/components/Submit';
+import { useAddDealContext } from '@/contexts/addDealContext';
+import { submitDealAction } from './actions';
+import { NewDealType } from '@/schemas';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
 export default function ReviewForm() {
+  const { newDealData, resetData } = useAddDealContext();
+  const router = useRouter();
+
+  const { name, link, coupon, discount, contactName, contactEmail } =
+    newDealData;
+
+  const handleFormSubmit = async (formData: FormData) => {
+    // submit to action
+    const { success, errorMsg, redirect } = await submitDealAction(
+      newDealData as NewDealType
+    );
+    if (success) {
+      toast.success('Deal submitted successfully');
+      resetData();
+    } else if (errorMsg) {
+      toast.error(errorMsg);
+    }
+    if (redirect) {
+      router.push(redirect);
+    }
+  };
+
   return (
-    <form className="flex flex-1 flex-col gap-2 items-stretch lg:max-w-[700px]">
-      <p className="text-xl md:text-3xl">Name: </p>
+    <form
+      action={handleFormSubmit}
+      className="flex flex-1 flex-col gap-2 items-stretch lg:max-w-[700px]"
+    >
+      <p className="text-xl md:text-3xl">Name: {name}</p>
       <p className="font-light text-white/90">
         Link:{' '}
         <a
-          href={''}
+          href={link}
           target="_blank"
           rel="noreferrer"
           className="font-normal underline hover:text-teal-500"
@@ -15,10 +47,11 @@ export default function ReviewForm() {
           Link
         </a>
       </p>
-      <p className="text-white/90">Coupon:</p>
-      <p className="text-white/90">Discount:</p>
-      <p className="text-white/90">Contact Name:</p>
-      <p className="text-white/90">Contact Email:</p>
+      <p className="text-white/90">Coupon: {coupon}</p>
+      <p className="text-white/90">Discount: {discount}</p>
+      <p className="text-white/90">Contact Name: {contactName}</p>
+      <p className="text-white/90">Contact Email: {contactEmail}</p>
+      <SubmitButton text="Submit" />
     </form>
   );
 }
--- src/app/add/review/actions.tsx
+++ src/app/add/review/actions.tsx
@@ -1,3 +1,43 @@
 'use server';
 
-export const submitDealAction = async () => {};
+import { NewDealType, stepOneSchema } from '@/schemas';
+import { AddDealRoutes } from '@/types';
+
+export type SubmitDealActionReturnType = {
+  redirect?: AddDealRoutes;
+  errorMsg?: string;
+  success?: boolean;
+};
+
+export const submitDealAction = async (
+  deal: NewDealType
+): Promise<SubmitDealActionReturnType> => {
+  const stepOneValidated = await stepOneSchema.safeParseAsync(deal);
+  if (!stepOneValidated.success) {
+    return {
+      redirect: AddDealRoutes.PRODUCT_INFO,
+      errorMsg: 'Please validate product info',
+    };
+  }
+
+  const stepTwoValidated = await stepOneSchema.safeParseAsync(deal);
+  if (!stepTwoValidated.success) {
+    return {
+      redirect: AddDealRoutes.COUPON_DETAILS,
+      errorMsg: 'Please validate coupon details',
+    };
+  }
+
+  const stepThreeValidated = await stepOneSchema.safeParseAsync(deal);
+  if (!stepThreeValidated.success) {
+    return {
+      redirect: AddDealRoutes.CONTACT_INFO,
+      errorMsg: 'Please validate contact info',
+    };
+  }
+
+  return {
+    success: true,
+    redirect: AddDealRoutes.PRODUCT_INFO,
+  };
+};
